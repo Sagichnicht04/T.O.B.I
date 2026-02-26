c1849ba38659275035c769427fed4ac7d701ad3b
JasminHoffmann <hoffmannjasmin634@gmail.com>
2026-02-26 16:20:06 +0100

    Added Befehle

--- main/src/main/java/flo/jasmin/projekt/Befehle.java
+++ main/src/main/java/flo/jasmin/projekt/Befehle.java
@@ -1,5 +1,16 @@
 package main.java.flo.jasmin.projekt;
 
 public enum Befehle {
-    
+    HOCH("Norden"),
+    RUNTER("Süden"),
+    LINKS("Westen"),
+    RECHTS("Osten"),
+    KOCHEN("Kochen"),
+    REDEN("Reden"),
+    ANGRIFF("Angreifen"),
+    KAUFEN("Kaufen"),
+    CAMPEN("Campen"),
+    SPEICHERN("Speichern"),
+    HILFE("Hilfe"),
+    KREATURAUSSTATTEN("Kreatur ausstatten")
 }
