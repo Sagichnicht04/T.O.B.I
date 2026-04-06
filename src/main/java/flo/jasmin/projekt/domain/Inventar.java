e923873d7c9ed47558198e7db989901ea01f9d26
Jasmin Hoffmann <jasminhoffmann436@gmail.com>
2026-04-06 19:54:33 +0200

    Zutaten Map im Inventar hinzugefügt. Methode zum Prüfen ob genug Zutaten im Inventar vorhanden sind und zum Entfernen. + paar Tests, die geenau das Überprüfen

--- src/main/java/flo/jasmin/projekt/domain/Inventar.java
+++ src/main/java/flo/jasmin/projekt/domain/Inventar.java
@@ -1,13 +1,16 @@
 package flo.jasmin.projekt.domain;
 
 import java.util.ArrayList;
+import java.util.Map;
 
+import flo.jasmin.projekt.domain.Exceptions.NichtGenugZutatenImInventar;
 import flo.jasmin.projekt.domain.Gegenstaende.Gegenstand;
 import flo.jasmin.projekt.domain.Gegenstaende.Zutat;
 
 public class Inventar {
     private int erspartes;
     private ArrayList<Gegenstand> gegenstände;
+    private Map<Zutat, Integer> zutaten;
     private int zeltkapazität;
     private float kochtopfMultiplikator;
 
@@ -59,4 +62,41 @@ public class Inventar {
     public void fügeGegenständeHinzu(ArrayList<Gegenstand> gegenstände){
         this.gegenstände.addAll(gegenstände);
     }
+    public Map<Zutat, Integer> getZutaten() {
+        return zutaten;
+    }
+    public void setZutaten(Map<Zutat, Integer> zutaten) {
+        this.zutaten = zutaten;
+    }    
+
+    public void fügeZutatenHinzu(Map<Zutat, Integer> zutaten){
+        for(Zutat z : zutaten.keySet()){
+            if(this.zutaten.containsKey(z)){
+                this.zutaten.put(z, this.zutaten.get(z) + zutaten.get(z));
+            } else {
+                this.zutaten.put(z, zutaten.get(z));
+            } 
+        }
+    }
+
+    public void entferneZutaten(Map<Zutat, Integer> zutaten) throws NichtGenugZutatenImInventar{
+        checkGenugZutatenImImventar(zutaten);
+        for (Zutat z : zutaten.keySet()){
+            this.zutaten.put(z, this.zutaten.get(z) - zutaten.get(z));
+            if (this.zutaten.get(z) == 0){
+                this.zutaten.remove(z);
+            }
+        }
+    }
+
+
+    //sogenante "guard-Methode" sie ist nur zum absichern da. Deswegen wirft sie einen Fehler oder eben nicht
+    public void checkGenugZutatenImImventar(Map<Zutat, Integer> zutaten) throws NichtGenugZutatenImInventar{
+        for (Zutat z : zutaten.keySet()){
+            if(!this.zutaten.containsKey(z) || zutaten.get(z) > this.zutaten.get(z)){
+                throw new NichtGenugZutatenImInventar();
+            }
+        }
+    }
+
 }
