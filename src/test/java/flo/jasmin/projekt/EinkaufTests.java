670b0be81a61956c71677b9f2aac76b25e2ddcac
Florian Zürner <florian.zuerner@sap.com>
2026-04-23 15:31:06 +0200

    Add Test Case

--- src/test/java/flo/jasmin/projekt/EinkaufTests.java
+++ src/test/java/flo/jasmin/projekt/EinkaufTests.java
@@ -2,9 +2,12 @@ package flo.jasmin.projekt;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import flo.jasmin.projekt.domain.Gegenstaende.Ausstattungen.Ausstattung;
+import flo.jasmin.projekt.domain.Gegenstaende.Ausstattungen.Holzschwert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -24,21 +27,26 @@ public class EinkaufTests {
     @BeforeEach
     void setup(){
         testSpiel = new Spiel();
-        testSpiel.getTeam().getInventar().setErspartes(4);
+        testSpiel.getTeam().getInventar().setErspartes(12);
     }
 
     @Test
     void erfolgreicherKauf(){
         Map<Gegenstand, Integer> auswahl = new HashMap<>();
         auswahl.put(new Karotte(), 2);
+        auswahl.put(new Holzschwert(), 1);
+
         int gesamtPreis = Dorf.gesamtpreisBerechnen(auswahl);
         testSpiel.setEinkauf(new Einkauf(auswahl, gesamtPreis));
         testSpiel.setStatus(Status.EINKAUF);
         testSpiel.spieleBefehl(Befehl.JA, null);
 
-        Map<Zutat, Integer> korrektesErgebnis = new HashMap<>();
-        korrektesErgebnis.put(new Karotte(), 2);
-        assertEquals(korrektesErgebnis, testSpiel.getTeam().getInventar().getZutaten());
+        Map<Zutat, Integer> korrektesErgebnisZutaten = new HashMap<>();
+        korrektesErgebnisZutaten.put(new Karotte(), 2);
+        ArrayList<Ausstattung> korrektesErgebnisAusruestung = new ArrayList<>();
+        korrektesErgebnisAusruestung.add(new Holzschwert());
+        assertEquals(korrektesErgebnisZutaten, testSpiel.getTeam().getInventar().getZutaten());
+        assertEquals(korrektesErgebnisAusruestung, testSpiel.getTeam().getInventar().getAusruestung());
     }
 
     @Test
